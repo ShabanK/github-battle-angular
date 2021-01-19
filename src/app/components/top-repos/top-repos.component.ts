7fda6cbaed61dc55a9d7b2d23f17467c1187f69b
ShabanK <shabankhawar99@gmail.com>
2021-01-19 16:18:07 +0500

    created a card component...css is hard

--- src/app/components/top-repos/top-repos.component.ts
+++ src/app/components/top-repos/top-repos.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { MOCKDATA } from "../mockdata";
 @Component({
   selector: 'app-top-repos',
   templateUrl: './top-repos.component.html',
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class TopReposComponent implements OnInit {
   languages: String[] = ["All", "Java", "Javascript", "Python", "Ruby"]
   // `https://api.github.com/search/repositories?q=stars:>1+language:${language}&sort=stars&order=desc&type=Repositories`
+  //delete later
+  siteurl='https://github.com/freeCodeCamp' 
+  image="https://avatars0.githubusercontent.com/u/9892522?v=4" 
+  title="freecodecamp"
+  mockdata = MOCKDATA;
+  //
   constructor() { }
 
   ngOnInit() {
